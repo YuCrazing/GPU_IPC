1d6f16e164c0c6384bfbdefa556f45ccdd72f4d8
yucrazing <723284893@qq.com>
2025-05-17 12:48:19 +0800

    Add building command

---
+++ build_linux.sh
@@ -0,0 +1,16 @@
+# build_type="Debug"
+# build_type="RelWithDebInfo"
+build_type="Release"
+
+mkdir build; cd build
+# cmake Could NOT find OpenGL
+# https://github.com/ContinuumIO/anaconda-issues/issues/8779
+cmake -S .. -DCMAKE_BUILD_TYPE=$build_type 
+cmake --build . --parallel 10 --config $build_type
+build_status=$?
+cd ..
+if [ $build_status -eq 0 ]; then
+    ./build/gipc
+else
+    echo "Build failed."
+fi
