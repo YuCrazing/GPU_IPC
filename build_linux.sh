d3166259191df27bf4f67d36989e0f93b7011df9
Yu Zhang <723284893@qq.com>
2025-09-01 16:53:03 +0800

    Fix cuda env variables

--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,7 @@
+export CUDA_HOME=/usr/local/cuda-12.4
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:${LD_LIBRARY_PATH}
+
 # build_type="Debug"
 # build_type="RelWithDebInfo"
 build_type="Release"
