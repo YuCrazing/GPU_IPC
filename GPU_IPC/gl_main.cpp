d0387b71523685e743daac904b312fd8971013e3
Yu Zhang <723284893@qq.com>
2025-09-15 11:43:11 +0800

    Clean cloth sim env

--- GPU_IPC/gl_main.cpp
+++ GPU_IPC/gl_main.cpp
@@ -698,10 +698,10 @@ void initScene1(int argc, char** argv)
     //string filePath(scene_file_path);
     tetMesh.load_tetrahedraMesh(
         assets_dir + "tetMesh/bunny2.msh", 0.2, make_double3(0, 0.65, 0));
-    tetMesh.load_tetrahedraMesh(
-        assets_dir + "tetMesh/bunny2.msh", 0.2, make_double3(0, -0, 0));
-    //tetMesh.load_triMesh(assets_dir + filePath, 1, make_double3(0, -0, 0), 0);
-    //tetMesh.boundaryTypies[0] = 1;
+    // tetMesh.load_tetrahedraMesh(
+    //     assets_dir + "tetMesh/bunny2.msh", 0.2, make_double3(0, -0, 0));
+    tetMesh.load_triMesh(assets_dir + "triMesh/tricloth.obj", 1, make_double3(0, -0, 0), 0);
+    // tetMesh.boundaryTypies[0] = 1;
     //__GEIGEN__::__set_Mat_val(tetMesh.constraints[0], 0, 0, 0, 0, 0, 0, 0, 0, 0);
     //tetMesh.constraints[0] =
     tetMesh.getSurface();
